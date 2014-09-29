78cb67dc287b689b8c9483fd898b25ae8a9ad1a9
Jonas Knipper <jonas@robocomobo.com>
2014-09-29 18:14:51 +0200

    Site updated at 2014-09-29 16:14:51 UTC

---
+++ downloads/code/ruby/gosu.rb
@@ -0,0 +1,12 @@
+require 'rubygems' # only necessary in Ruby 1.8
+require 'gosu'
+
+class MyWindow < Gosu::Window
+  def initialize
+   super(640, 480, false)
+   self.caption = 'Hello World!'
+  end
+end
+
+window = MyWindow.new
+window.show
